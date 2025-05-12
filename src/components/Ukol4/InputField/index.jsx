f42f28eb53ffeeda2c2a2d6b1d78c6b29d3241bf
Lenka Koubková <lvseteckova@gmail.com>
2025-05-12 15:33:47 +0200

    výsledky

--- src/components/Ukol4/InputField/index.jsx
+++ src/components/Ukol4/InputField/index.jsx
@@ -1,12 +1,15 @@
 import './style.css';
 
 export const InputField = ({ label, type, value, onValueChange }) => {
+  
   return (
     <div className="input-field">
       <label className="input-field__label">{label}</label>
       <input
         className="input-field__input"
         type={type}
+        onChange={(e) => onValueChange (e.target.value)}
+        value={value}
       />
     </div>
   );
